c64591e9ab7bcfa257ec1705f037a83d695ca9fa
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2023-02-09 16:44:42 +0300

    broker api response adjusted

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,11 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type jsonResponse struct {
-	Error bool
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the Broker",
+	}
 
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
 }
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -20,6 +20,7 @@ func (app *Config) routes() http.Handler {
 	}))
 
 	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("/", app.Broker)
 
 	return mux
 }
