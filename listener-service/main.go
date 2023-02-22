f71a17f5d5407c12ff85ab998f54702347f9d9dd
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2023-02-22 17:32:16 +0300

    rabbit mq connection adjusted

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,11 +1,56 @@
 package main
 
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
 func main() {
 	// connect rabbit mq
-	
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("We are connect RabbitMQ!!")
+
 	// start listening for messages
 
 	// tetikleyici yarat mesajları alacak ve işleyecek.
 
 	// kuyruğu izleyecek ve eventleri tetikleyecek.
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
