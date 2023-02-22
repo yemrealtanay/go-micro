3e58525a4823b5e17849cddc408406f57994720a
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2023-02-22 17:07:41 +0300

    rabbit mq image added and listener service files created

---
+++ listener-service/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// connect rabbit mq
+	
+	// start listening for messages
+
+	// tetikleyici yarat mesajları alacak ve işleyecek.
+
+	// kuyruğu izleyecek ve eventleri tetikleyecek.
+}
